fc2d0d9c2b9f2286be6c36f61b24bec61ac26b1d
Sang Cá Mập <55171560+sangcamap@users.noreply.github.com>
2021-09-30 21:57:36 +0700

    fix trang main + thêm event click cho trang menu

--- app.js
+++ app.js
@@ -3,10 +3,21 @@ window.scroll({
     left: 0, 
     behavior: 'smooth'
   });
-  
-  // Scroll certain amounts from current position 
   window.scrollBy({ 
     top: 100, // could be negative value
     left: 0, 
     behavior: 'smooth' 
   });
+
+
+  
+  var fullimgbox = document.getElementById("fullimgbox");
+  var fullimg = document.getElementById("fullimg");
+
+  function openFullimg(img){
+      fullimgbox.style.display="flex";
+      fullimg.src=img;
+  }
+  function closeFullimg(){
+      fullimgbox.style.display="none";
+  }
